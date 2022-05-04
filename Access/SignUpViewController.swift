c165d381b7d80577380ab0e93da61bde737fa1a3
ivan776 <mendozaivan1224@gmail.com>
2022-05-03 21:58:27 -0700

    users can create accounts

--- Access/SignUpViewController.swift
+++ Access/SignUpViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import Parse
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "mainPageSegue", sender: nil)
+               
+            } else {
+                print("Error: \(error?.localizedDescription)")
+                let alertDisapperTimeInSeconds = 1.5
+                let alert = UIAlertController(title: nil, message: "Sign Up Error!", preferredStyle: .actionSheet)
+                self.present(alert, animated: true)
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + alertDisapperTimeInSeconds) {
+                  alert.dismiss(animated: true)
+                }
+            }
+        }
     }
     
 
